24813d319d93b32c378ad23e1ad5b3b60117fad0
davispc10 <davispc100@gmail.com>
2019-06-05 23:41:53 -0300

    Adicionando projeto de node api

--- huntweb/src/components/Header/index.js
+++ huntweb/src/components/Header/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import "./styles.css";
 
 // eslint-disable-next-line react/react-in-jsx-scope
-const Header = () => <header id="main-header">JSHunt</header>;
+const Header = () => <header id="main-header"> JSHunt </header>;
 
 export default Header;
