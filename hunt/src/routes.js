518fbd9232f4d345703b2303aef3cbdff49556cb
davispc10 <davispc100@gmail.com>
2019-06-19 03:33:10 -0300

    Finalização de app mobile

---
+++ hunt/src/routes.js
@@ -0,0 +1,11 @@
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+import Main from "./pages/main";
+import Product from "./pages/product";
+
+export default createAppContainer(
+  createStackNavigator({
+    Main,
+    Product
+  })
+);
